36d14882394d6bcd4b644d2b64e659f9dfaa8510
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-11 12:17:23 +0700

    render FoodDisplay

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "@/component/Header";
 import ExploreMenu from "@/component/ExploreMenu";
+import FoodDisplay from "@/component/FoodDisplay";
 const Home = () => {
   const [category, setCategory] = useState("All");
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 };
