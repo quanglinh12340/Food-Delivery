b84f4736b1c3392907fffae21bd4dc0d3c6dbe43
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-11 13:55:46 +0700

    render Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import PlaceOder from "./pages/PlaceOder";
+import Footer from "./component/Footer";
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/oder" element={<PlaceOder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/oder" element={<PlaceOder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
