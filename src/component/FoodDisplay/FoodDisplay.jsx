138eb6cd054f7c285333e034adb7959bbb4b3e24
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-11 12:15:37 +0700

    FoodDisplay component

---
+++ src/component/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { PropTypes } from "prop-types";
+import "./FoodDisplay.css";
+import { StoreContext } from "@/context/StoreContext";
+import FoodItem from "../FoodItem";
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dished near you </h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                image={item.image}
+                price={item.price}
+                description={item.description}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+FoodDisplay.propTypes = {
+  category: PropTypes.string,
+};
+export default FoodDisplay;
