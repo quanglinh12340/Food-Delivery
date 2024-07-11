0ee78dcd05ff05862d9a1dac807962ff2626946b
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-11 07:29:55 +0700

    ExploreMenu component

---
+++ src/component/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "@/assets";
+
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p>
+        {" "}
+        Choose from a diverse menu featuring a delectable array of dishes
+        crafted with the finest ingredients and culinary expertise. Our mission
+        is to satisfy your cravings and elevate your dining experience, one
+        delicious meat at the time
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => (
+          <div
+            key={index}
+            className="explore-menu-list-item"
+            onClick={() =>
+              setCategory((prev) => {
+                return prev === item.menu_name ? "All" : item.menu_name;
+              })
+            }
+          >
+            <img
+              className={category === item.menu_name ? "active" : ""}
+              src={item.menu_image}
+              alt=""
+            />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
