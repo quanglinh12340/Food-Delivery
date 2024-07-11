de0c740a5c7fb35958465e0c439488052db57a5b
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-11 07:32:33 +0700

    use router render page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar from "./component/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import PlaceOder from "./pages/PlaceOder";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/oder" element={<PlaceOder />} />
+      </Routes>
     </div>
   );
 };
